7c9e918bee9ac3910e5b098a00c703f39a2fb5f0
thekanon <dlengjs12@naver.com>
2024-11-07 03:29:58 +0900

    에코시스템 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,10 @@ module.exports = {
       instances: 2,
       autorestart: true,
       exec_mode: "cluster",
+      output: "./logs/out.log",
+      error: "./logs/error.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      merge_logs: true,
     },
   ],
 };
